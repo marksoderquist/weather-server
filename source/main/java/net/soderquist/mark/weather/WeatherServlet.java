07e7fe509e01661fd423ff615e515e1ea589551b
Mark Soderquist <mark@soderquist.net>
2015-11-21 14:22:59 -0700

    Refactor to use PUT instead of GET

--- source/main/java/net/soderquist/mark/weather/WeatherServlet.java
+++ source/main/java/net/soderquist/mark/weather/WeatherServlet.java
@@ -3,6 +3,7 @@ package net.soderquist.mark.weather;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ public class WeatherServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -2358024754122910316L;
 
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat( "yyyy-MM-dd hh:mm:ss a" );
 
 	private WeatherStation station;
 
@@ -24,62 +25,34 @@ public class WeatherServlet extends HttpServlet {
 	 */
 	@Override
 	public void init() throws ServletException {
-		Log.write(Log.TRACE, "Starting Weather Reader...");
+		Log.write( Log.TRACE, "Starting Weather Reader..." );
 
 		station = new WeatherStation();
-		getServletContext().setAttribute("wxstation", station);
+		station.put( "temperature-unit", WeatherStation.DEGREE + "F" );
+		station.put( "humidity-unit", "%" );
+		station.put( "pressure-unit", "inHg" );
+		station.put( "wind-direction-unit", WeatherStation.DEGREE );
+		station.put( "wind-unit", "mph" );
+		station.put( "rain-total-daily-unit", "in" );
+		station.put( "rain-rate-unit", "in/hr" );
 
-		Log.write(Log.INFO, "Weather Reader started.");
+		getServletContext().setAttribute( "wxstation", station );
+
+		Log.write( Log.INFO, "Weather Reader started." );
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call this method with:
-		// http://<host>[:<port>]/<context>/station/put?id=21&t=42.1&h=57&p=29.92
+	protected void doPut( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+		Properties properties = new Properties();
+		properties.load( request.getInputStream() );
 
-		// Log.write( "Station ID: ", request.getParameter( "id" ) );
-		// Log.write( "Timestamp: ", request.getParameter( "ts" ) );
-		// Log.write( "Temperature: ", request.getParameter( "t" ) );
-		// Log.write( "Humidity: ", request.getParameter( "h" ) );
-		// Log.write( "Pressure: ", request.getParameter( "p" ) );
-		// Log.write( "Wind direction: ", request.getParameter( "wd" ) );
-		// Log.write( "Wind instant: ", request.getParameter( "wi" ) );
-		// Log.write( "Wind sustain: ", request.getParameter( "ws" ) );
-		// Log.write( "Rain rate: ", request.getParameter( "rr" ) );
-		// Log.write( "Rain total daily: ", request.getParameter( "rd" ) );
+		// Store all the values.
+		for( Object key : properties.keySet() ) {
+			station.put( key.toString(), properties.get( key ).toString() );
+		}
 
-		Date timestamp = null;
-		String timestampValue = request.getParameter("ts");
-		if (timestampValue != null) timestamp = new Date(Long.parseLong(timestampValue));
-		station.put("timestamp", timestamp == null ? null : DATE_FORMAT.format(timestamp));
-
-		station.put("temperature", request.getParameter("t"));
-		station.put("temperature-unit", WeatherStation.DEGREE + "F");
-
-		station.put("humidity", request.getParameter("h"));
-		station.put("humidity-unit", "%");
-
-		station.put("pressure", request.getParameter("p"));
-		station.put("pressure-unit", "inHg");
-
-		station.put("wind-direction", request.getParameter("wd"));
-		station.put("wind-direction-unit", WeatherStation.DEGREE);
-
-		station.put("wind-current", request.getParameter("wi"));
-		station.put("wind-10-min-max", request.getParameter("wmax10"));
-		station.put("wind-10-min-avg", request.getParameter("wavg10"));
-		station.put("wind-10-min-min", request.getParameter("wmin10"));
-		station.put("wind-2-min-max", request.getParameter("wmax2"));
-		station.put("wind-2-min-avg", request.getParameter("wavg2"));
-		station.put("wind-2-min-min", request.getParameter("wmin2"));
-		station.put("wind-unit", "mph");
-
-		station.put("rain-total-daily", request.getParameter("rd"));
-		station.put("rain-total-daily-unit", "in");
-
-		station.put("rain-rate", request.getParameter("rr"));
-		station.put("rain-rate-unit", "in/hr");
+		// Reformat the timestamp.
+		station.put( "timestamp", DATE_FORMAT.format( new Date( Long.parseLong( properties.getProperty( "timestamp" ) ) ) ) );
 	}
 
 	/**
@@ -87,8 +60,8 @@ public class WeatherServlet extends HttpServlet {
 	 */
 	@Override
 	public void destroy() {
-		Log.write(Log.TRACE, "Stopping Weather Reader...");
-		Log.write(Log.INFO, "Weather Reader stopped.");
+		Log.write( Log.TRACE, "Stopping Weather Reader..." );
+		Log.write( Log.INFO, "Weather Reader stopped." );
 	}
 
 }
