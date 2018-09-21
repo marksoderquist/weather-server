f5db4fdf506cb425f8429d62c13050298b78e7c9
Mark Soderquist <mark@soderquist.net>
2018-09-21 12:42:16 -0600

    Use unit values from server

--- client/src/App.js
+++ client/src/App.js
@@ -82,52 +82,60 @@ class Body extends Component {
         return (
             <div className="content">
                 <div className="column">
-                    <div className="temperature">{parseFloat(this.props.weather.temperature).toFixed(1)}&deg;</div>
+                    <div className="temperature">{parseFloat(this.props.weather.temperature).toFixed(1)}<span className="tempunit">{this.props.weather.temperatureUnit}</span></div>
                     <div className="wind">
                         <table>
-                            <tr><td class="label">wind&nbsp;</td><td class="value">&nbsp;{parseFloat(this.props.weather.windTenMinAvg).toFixed(1)}</td><td class="unit">&nbsp;mph</td></tr>
-                            <tr><td class="label">gust&nbsp;</td><td class="value">&nbsp;{parseFloat(this.props.weather.windTenMinMax).toFixed(1)}</td><td class="unit">&nbsp;mph</td></tr>
+                            <tr>
+                                <td class="label">wind&nbsp;</td>
+                                <td class="value">&nbsp;{parseFloat(this.props.weather.windTenMinAvg).toFixed(1)}</td>
+                                <td class="unit">&nbsp;{this.props.weather.windSpeedUnit}</td>
+                            </tr>
+                            <tr>
+                                <td class="label">gust&nbsp;</td>
+                                <td class="value">&nbsp;{parseFloat(this.props.weather.windTenMinMax).toFixed(1)}</td>
+                                <td class="unit">&nbsp;{this.props.weather.windSpeedUnit}</td>
+                            </tr>
                             <tr>
                                 <td className="label">from&nbsp;</td>
                                 <td className="value">&nbsp;{this.props.weather.windCardinalTenMinAvg}</td>
-                                <td className="unit">&nbsp;{parseFloat(this.props.weather.windDirectionTenMinAvg).toFixed(0)}&deg;</td>
+                                <td className="unit">&nbsp;{parseFloat(this.props.weather.windDirectionTenMinAvg).toFixed(0)}{this.props.weather.windDirectionUnit}</td>
                             </tr>
                         </table>
                     </div>
                     <Separator/>
-                    <NumberField name='Temperature Trend' value={this.props.weather.temperatureTrend} unit='&deg;F/h' fixed='1'/>
-                    <NumberField name='Wind Speed Trend' value={this.props.weather.windSpeedTrend} unit=' mph/h' fixed='1'/>
+                    <NumberField name='Temperature Trend' value={this.props.weather.temperatureTrend} unit={this.props.weather.temperatureTrendUnit} fixed='1'/>
+                    <NumberField name='Wind Speed Trend' value={this.props.weather.windSpeedTrend} unit={this.props.weather.windSpeedTrendUnit} fixed='1'/>
                     <Separator/>
-                    <NumberField name='Temperature' value={this.props.weather.temperature} unit='&deg;' fixed='1'/>
-                    <NumberField name='Dew Point' value={this.props.weather.dewPoint} unit='&deg;' fixed='1'/>
-                    <NumberField name='Humidity' value={this.props.weather.humidity} unit='%'/>
-                    <NumberField name='Pressure' value={this.props.weather.pressure} unit=' inHg' fixed='2'/>
+                    <NumberField name='Temperature' value={this.props.weather.temperature} unit={this.props.weather.temperatureUnit} fixed='1'/>
+                    <NumberField name='Dew Point' value={this.props.weather.dewPoint} unit={this.props.weather.temperatureUnit} fixed='1'/>
+                    <NumberField name='Humidity' value={this.props.weather.humidity} unit={this.props.weather.humidityUnit}/>
+                    <NumberField name='Pressure' value={this.props.weather.pressure} unit={this.props.weather.pressureUnit} fixed='2'/>
                     <Separator/>
-                    <NumberField name='Wind Speed' value={this.props.weather.windTenMinAvg} unit=' mph'/>
-                    <NumberField name='Wind Gusts' value={this.props.weather.windTenMinMax} unit=' mph'/>
-                    <NumberField name='Wind Minimum' value={this.props.weather.windTenMinMin} unit=' mph'/>
-                    <NumberField name='Wind Direction' value={this.props.weather.windDirectionTenMinAvg} unit='&deg;F'/>
+                    <NumberField name='Wind Speed' value={this.props.weather.windTenMinAvg} unit={this.props.weather.windSpeedUnit}/>
+                    <NumberField name='Wind Gusts' value={this.props.weather.windTenMinMax} unit={this.props.weather.windSpeedUnit}/>
+                    <NumberField name='Wind Minimum' value={this.props.weather.windTenMinMin} unit={this.props.weather.windSpeedUnit}/>
+                    <NumberField name='Wind Direction' value={this.props.weather.windDirectionTenMinAvg} unit={this.props.weather.windDirectionUnit}/>
                     <Separator/>
-                    <NumberField name='Daily Rain' value={this.props.weather.rainTotalDaily} unit=' in' fixed='2'/>
-                    <NumberField name='Rain Rate' value={this.props.weather.rainRate} unit=' in/h' fixed='2'/>
+                    <NumberField name='Daily Rain' value={this.props.weather.rainTotalDaily} unit={this.props.weather.rainUnit} fixed='2'/>
+                    <NumberField name='Rain Rate' value={this.props.weather.rainRate} unit={this.props.weather.rainRateUnit} fixed='2'/>
                     <Separator/>
                     <Separator/>
-                    <NumberField name='Wind Chill' value={this.props.weather.windChill} unit='&deg;' fixed='1'/>
-                    <NumberField name='Head Index' value={this.props.weather.heatIndex} unit='&deg;' fixed='1'/>
+                    <NumberField name='Wind Chill' value={this.props.weather.windChill} unit={this.props.weather.temperatureUnit} fixed='1'/>
+                    <NumberField name='Head Index' value={this.props.weather.heatIndex} unit={this.props.weather.temperatureUnit} fixed='1'/>
                     <Separator/>
-                    <NumberField name='Humidity Trend' value={this.props.weather.humidityTrend} unit='%/h' fixed='1'/>
-                    <NumberField name='Pressure Trend' value={this.props.weather.pressureTrend} unit=' inHg/h' fixed='3'/>
+                    <NumberField name='Humidity Trend' value={this.props.weather.humidityTrend} unit={this.props.weather.humidityTrendUnit} fixed='1'/>
+                    <NumberField name='Pressure Trend' value={this.props.weather.pressureTrend} unit={this.props.weather.pressureTrendUnit} fixed='3'/>
                     <Separator/>
-                    <NumberField name='Maximum Wind Speed (10 min)' value={this.props.weather.windTenMinMax} unit=' mph' fixed='1'/>
-                    <NumberField name='Average Wind Speed (10 min)' value={this.props.weather.windTenMinAvg} unit=' mph' fixed='1'/>
-                    <NumberField name='Minimum Wind Speed (10 min)' value={this.props.weather.windTenMinMin} unit=' mph' fixed='1'/>
+                    <NumberField name='Maximum Wind Speed (10 min)' value={this.props.weather.windTenMinMax} unit={this.props.weather.windSpeedUnit} fixed='1'/>
+                    <NumberField name='Average Wind Speed (10 min)' value={this.props.weather.windTenMinAvg} unit={this.props.weather.windSpeedUnit} fixed='1'/>
+                    <NumberField name='Minimum Wind Speed (10 min)' value={this.props.weather.windTenMinMin} unit={this.props.weather.windSpeedUnit} fixed='1'/>
                     <Separator/>
-                    <NumberField name='Maximum Wind Speed (2 min)' value={this.props.weather.windTwoMinMax} unit=' mph' fixed='1'/>
-                    <NumberField name='Average Wind Speed (2 min)' value={this.props.weather.windTwoMinAvg} unit=' mph' fixed='1'/>
-                    <NumberField name='Minimum Wind Speed (2 min)' value={this.props.weather.windTwoMinMin} unit=' mph' fixed='1'/>
+                    <NumberField name='Maximum Wind Speed (2 min)' value={this.props.weather.windTwoMinMax} unit={this.props.weather.windSpeedUnit} fixed='1'/>
+                    <NumberField name='Average Wind Speed (2 min)' value={this.props.weather.windTwoMinAvg} unit={this.props.weather.windSpeedUnit} fixed='1'/>
+                    <NumberField name='Minimum Wind Speed (2 min)' value={this.props.weather.windTwoMinMin} unit={this.props.weather.windSpeedUnit} fixed='1'/>
                     <Separator/>
-                    <NumberField name='Instant Wind Speed' value={this.props.weather.windSpeed} unit=' mph'/>
-                    <NumberField name='Instant Wind Direction' value={this.props.weather.windDirection} unit='&deg;'/>
+                    <NumberField name='Instant Wind Speed' value={this.props.weather.windSpeed} unit={this.props.weather.windSpeedUnit}/>
+                    <NumberField name='Instant Wind Direction' value={this.props.weather.windDirection} unit={this.props.weather.windDirectionUnit}/>
                     <Separator/>
                     <NumberField name='Timestamp' value={this.props.weather.timestamp} unit=' ms'/>
                 </div>
@@ -141,7 +149,7 @@ class NumberField extends Component {
         return (
             <div className="weather-field">
                 <div className="weather-field-prompt">{this.props.name}</div>
-                <div className="weather-field-value">{parseFloat(this.props.value).toFixed(this.props.fixed)}{this.props.unit}</div>
+                <div className="weather-field-value">{parseFloat(this.props.value).toFixed(this.props.fixed)} {this.props.unit}</div>
             </div>
         );
     }
@@ -167,11 +175,14 @@ function toDatestamp(time) {
     const second = pad(date.getSeconds(), 2);
     const ampm = isAmOrPm(date.getHours());
 
-    return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second + ' ' + ampm;
+    //if (hour === '00') hour = '12';
+
+    return year + '-' + month + '-' + day + ' ' + (hour === '00' ? 12 : hour) + ':' + minute + ':' + second + ' ' + ampm;
 }
 
+// Hour must be between 0 and 23
 function isAmOrPm(h) {
-    return h < 13 ? 'am' : 'pm';
+    return h < 12 ? 'am' : 'pm';
 }
 
 function pad(n, z, p = 0) {
