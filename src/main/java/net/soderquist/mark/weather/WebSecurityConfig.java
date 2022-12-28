2b30d308d1cad772a0e3a50567b54119b0d5e73c
msoderquist <msoderquist@mastercontrol.com>
2022-12-28 14:33:45 -0700

    Fix web security warning

--- src/main/java/net/soderquist/mark/weather/WebSecurityConfig.java
+++ src/main/java/net/soderquist/mark/weather/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package net.soderquist.mark.weather;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +13,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.lang.invoke.MethodHandles;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+	private final Logger log = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
+
 	@Bean
 	protected SecurityFilterChain configure( HttpSecurity http ) throws Exception {
-		http.csrf().ignoringRequestMatchers( request -> request.getMethod().equals( "GET" ) && request.getServletPath().equals( "/station" ) );
+		http.csrf().ignoringRequestMatchers( request -> {
+			log.info( request.getMethod() + " -> " + request.getServletPath() );
+			return request.getMethod().equals( "GET" ) && request.getServletPath().startsWith( "/station" );
+		} );
 		http.authorizeHttpRequests().requestMatchers(HttpMethod.GET, "/station").permitAll().requestMatchers( HttpMethod.PUT, "/station" ).authenticated().and().httpBasic( withDefaults() );
 		return http.build();
 	}
